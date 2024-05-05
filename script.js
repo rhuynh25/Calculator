677f17f89b9eda0f785ff3ee144947a3816483af
rhuynh25 <rhuynh125@gmail.com>
2024-05-05 14:45:25 -0400

    final commit

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 let currentInput = ''; // Making the current input empty but stores the input when you press the numbers
 
+// Function allows you to clear the screen
 function appearToScreen(value) {
   currentInput += value;
   document.getElementById('result').value = currentInput;
-} // This function adds the value
+}
 
 function clearScreen() {
   currentInput = '';
   document.getElementById('result').value = '';
-} // This function clears the screen
+}
 
+// Shows error if input isn't a number. Ex. "100/0"
 function calculate() {
   try {
     const result = eval(currentInput);
     if (isNaN(result) || !isFinite(result)) {
-      throw new Error('Invalid input');
+      throw new Error('Invalid input'); // Throwing back the error to the caller
     } 
     document.getElementById('result').value = result;
     currentInput = result.toString();
-  } catch (error) {
-    alert(error.message);
+    document.getElementById('error-message').textContent = ''; 
+  } catch (error) { // Catching the error in the function and handling it
+    document.getElementById('error-message').textContent = error.message; 
     clearScreen();
-  } 
-} // Shows error if input isn't a number. Ex. "100/0"
+  } // Display error message
+}
+
 
 
 // This section handles keyboard input
 function handleKeyboardInput(event) {
   const key = event.key;
-  if (/\d/.test(key)) {
+  if (/\d/.test(key)) { //implemented regex to check if the key is a number
     appearToScreen(key);
   } else if (key === '.' || key === '+' || key === '-' || key === '*' || key === '/') {
     appearToScreen(key);
